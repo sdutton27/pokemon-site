2b6689f38404fd021a05837c3a302c780029b80a
Simon Dutton <simonpcdutton@gmail.com>
2023-04-13 12:00:29 -0400

    adding day-2

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from config import Config
-from .models import db # new with forms
+from .models import db, User # new with forms
 from flask_migrate import Migrate # new with forms
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)  
@@ -9,6 +10,14 @@ app.config.from_object(Config)
 # new section with forms
 migrate = Migrate(app,db)
 db.init_app(app) 
+
+login_manager = LoginManager(app)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+    
+# this will take you to the loginpage if you try to go to a route
+login_manager.login_view = 'login_page' 
 
 from . import routes
 from . import models # new with forms
--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
@@ -10,6 +11,8 @@ class Pokemon(db.Model):
     base_attack = db.Column(db.Integer, nullable=False)
     front_shiny_sprite = db.Column(db.String, nullable=False, unique=True)
     abilities = db.Column(db.String(100), nullable=False) # multiple abilities
+
+    trainer_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = True)
 
     def __init__(self, name, base_hp, base_defense, base_attack, front_shiny_sprite, abilities):
         self.name = name
@@ -22,3 +25,27 @@ class Pokemon(db.Model):
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
+
+# this name is actually lowercase
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(45), nullable=False)
+    last_name = db.Column(db.String(45), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    date_created = db.Column(db.DateTime, nullable = False, default=datetime.utcnow)
+    profile_pic = db.Column(db.String, nullable=True)
+    
+    pokemon = db.relationship('Pokemon', backref='trainer', lazy = True)
+
+    def __init__(self, first_name, last_name, email, password, profile_pic=''):
+        self.first_name = first_name.title()
+        self.last_name = last_name.title()
+        self.email = email
+        self.password = password
+
+        self.profile_pic = profile_pic
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit() # actually commits things
